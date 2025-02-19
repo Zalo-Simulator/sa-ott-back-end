9fc1bad7a7f529f1511427a2a9cd8c684aa214fd
doantrancaotri1108 <doantrancaotri1108@gmail.com>
2025-02-19 22:07:39 +0700

    add logging

---
+++ app/helpers/logging.py
@@ -0,0 +1,12 @@
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Usage
+# from app.helpers.logging import logger
+# logger.info(f"Your message")
+# logger.error(f"Your message")
+# logger.warning(f"Your message")
